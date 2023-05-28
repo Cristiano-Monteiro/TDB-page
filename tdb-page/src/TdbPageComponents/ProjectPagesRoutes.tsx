e15c16e111c842136521bc02aa47a64b574776a8
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2023-05-28 19:40:00 -0300

    Update build files - 5

--- tdb-page/src/TdbPageComponents/ProjectPagesRoutes.tsx
+++ tdb-page/src/TdbPageComponents/ProjectPagesRoutes.tsx
@@ -11,7 +11,7 @@ import Jbrowse from "./Pages/Jbrowse/Jbrowse";
 
 export default function ProjectPagesRoutes() {
   return (
-    <BrowserRouter basename="/TDB-page/">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<Home/>} />
