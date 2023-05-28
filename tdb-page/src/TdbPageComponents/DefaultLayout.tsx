4a1bf2a13d7a648d69baa0219162c0d1c75b7cdf
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2023-05-28 17:13:23 -0300

    Added opening screen on page

--- tdb-page/src/TdbPageComponents/DefaultLayout.tsx
+++ tdb-page/src/TdbPageComponents/DefaultLayout.tsx
@@ -1,17 +1,32 @@
 import { Outlet } from 'react-router-dom';
 
+import { useRef } from 'react';
+
 import './GlobalStyles.css';
+
+import OpeningScreen from './PageSections/OpeningScreen/OpeningScreen';
 
 import Navbar from '../TdbPageComponents/PageSections/Navbar/Navbar';
 
 //import Footer from ''; 
 
 export default function DefaultLayout() {
+  const main = useRef<HTMLDivElement>(null);
+
+  setTimeout(()=>{
+    if(main.current){
+      main.current.style.display = 'flex';
+    };
+  }, 2500);
+
   return (
     <>
-      <Navbar />
-      <Outlet />
-      {/* Footer */}
+      <OpeningScreen/>
+      <div className='main' ref={main}>
+        <Navbar />
+        <Outlet />
+        {/* Footer */}
+      </div>
     </>
   );
 };
